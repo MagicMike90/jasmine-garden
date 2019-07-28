b4bc6db5b60ca98d8b1576ea89dcf250c568adcf
Michael Luo <michael.luo0801@gmail.com>
2019-07-28 16:20:09 +1000

    init

---
+++ app/Main.tsx
@@ -0,0 +1,222 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import {
+  ActivityIndicator,
+  AsyncStorage,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+import uuid from 'uuid/v1';
+
+import Button from './components/Button';
+import Header from './components/Header';
+import Input from './components/Input';
+import List from './components/List';
+import SubTitle from './components/SubTitle';
+import { primaryGradientArray } from './constants/colors';
+
+const headerTitle = 'Todo';
+
+interface IMainState {
+  inputValue: string;
+  loadingItems: boolean;
+  allItems: {};
+  isCompleted: boolean;
+}
+
+export default class Main extends React.Component<{}, IMainState> {
+  public state = {
+    inputValue: '',
+    loadingItems: false,
+    allItems: {},
+    isCompleted: false,
+  };
+
+  public componentDidMount = () => {
+    this.loadingItems();
+  }
+
+  public newInputValue = (value) => {
+    this.setState({
+      inputValue: value,
+    });
+  }
+
+  public loadingItems = async () => {
+    try {
+      const allItems = await AsyncStorage.getItem('Todos');
+      this.setState({
+        loadingItems: true,
+        allItems: JSON.parse(allItems) || {},
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  public onDoneAddItem = () => {
+    const { inputValue } = this.state;
+    if (inputValue !== '') {
+      this.setState((prevState) => {
+        const id = uuid();
+        const newItemObject = {
+          [id]: {
+            id,
+            isCompleted: false,
+            text: inputValue,
+            createdAt: Date.now(),
+          },
+        };
+        const newState = {
+          ...prevState,
+          inputValue: '',
+          allItems: {
+            ...prevState.allItems,
+            ...newItemObject,
+          },
+        };
+        this.saveItems(newState.allItems);
+        return { ...newState };
+      });
+    }
+  }
+
+  public deleteItem = (id) => {
+    this.setState((prevState) => {
+      const allItems = prevState.allItems;
+      delete allItems[id];
+      const newState = {
+        ...prevState,
+        ...allItems,
+      };
+      this.saveItems(newState.allItems);
+      return { ...newState };
+    });
+  }
+
+  public completeItem = (id) => {
+    this.setState((prevState) => {
+      const newState = {
+        ...prevState,
+        allItems: {
+          ...prevState.allItems,
+          [id]: {
+            ...prevState.allItems[id],
+            isCompleted: true,
+          },
+        },
+      };
+      this.saveItems(newState.allItems);
+      return { ...newState };
+    });
+  }
+
+  public incompleteItem = (id) => {
+    this.setState((prevState) => {
+      const newState = {
+        ...prevState,
+        allItems: {
+          ...prevState.allItems,
+          [id]: {
+            ...prevState.allItems[id],
+            isCompleted: false,
+          },
+        },
+      };
+      this.saveItems(newState.allItems);
+      return { ...newState };
+    });
+  }
+
+  public deleteAllItems = async () => {
+    try {
+      await AsyncStorage.removeItem('Todos');
+      this.setState({ allItems: {} });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  public saveItems = (newItem) => {
+    const saveItem = AsyncStorage.setItem('Todos', JSON.stringify(newItem));
+  }
+
+  public render() {
+    const { inputValue, loadingItems, allItems } = this.state;
+
+    return (
+      <LinearGradient colors={primaryGradientArray} style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <View style={styles.centered}>
+          <Header title={headerTitle} />
+        </View>
+        <View style={styles.inputContainer}>
+          <SubTitle subtitle={'What\'s Next?'} />
+          <Input
+            inputValue={inputValue}
+            onChangeText={this.newInputValue}
+            onDoneAddItem={this.onDoneAddItem}
+          />
+        </View>
+        <View style={styles.list}>
+          <View style={styles.column}>
+            <SubTitle subtitle={'Recent Notes'} />
+            <View style={styles.deleteAllButton}>
+              <Button deleteAllItems={this.deleteAllItems} />
+            </View>
+          </View>
+
+          {loadingItems ? (
+            <ScrollView contentContainerStyle={styles.scrollableList}>
+              {Object.values(allItems)
+                .reverse()
+                .map((item) => (
+                  <List
+                    key={item.id}
+                    {...item}
+                    deleteItem={this.deleteItem}
+                    completeItem={this.completeItem}
+                    incompleteItem={this.incompleteItem}
+                  />
+                ))}
+            </ScrollView>
+          ) : (
+            <ActivityIndicator size="large" color="white" />
+          )}
+        </View>
+      </LinearGradient>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  centered: {
+    alignItems: 'center',
+  },
+  inputContainer: {
+    marginTop: 40,
+    paddingLeft: 15,
+  },
+  list: {
+    flex: 1,
+    marginTop: 70,
+    paddingLeft: 15,
+    marginBottom: 10,
+  },
+  scrollableList: {
+    marginTop: 15,
+  },
+  column: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  deleteAllButton: {
+    marginRight: 40,
+  },
+});
